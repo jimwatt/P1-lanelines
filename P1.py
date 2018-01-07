936c52c4986a2b263befd010f3203d9ad89856d5
James Watt <11396617+jimwatt@users.noreply.github.com>
2018-01-07 05:49:08 -0700

    First commit of code.  Just need to do write-up.

---
+++ P1.py
@@ -0,0 +1,177 @@
+
+# coding: utf-8
+
+##############################################################################################################
+# import packages
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+import cv2
+import math
+import os
+from moviepy.editor import VideoFileClip
+from IPython.display import HTML
+import utilities as ut
+
+###################################################################################################################
+# The main process_image() function takes an image as input, and returns the same image with lane lines annotated.
+def process_image(image,vertextype):
+
+    # Define parameters
+    kernel_size = 5         # size of Gaussian smoothing kernel
+    low_threshold = 50      # low-threshold for canny edge detection
+    high_threshold = 150    # high-threshold for canny edge detection
+    rho = 1                 # distance resolution in pixels of the Hough grid
+    theta = np.pi/180       # angular resolution in radians of the Hough grid
+    threshold = 5           # minimum number of votes (intersections in Hough grid cell)
+    min_line_length = 10    # minimum number of pixels making up a line
+    max_line_gap = 20       # maximum gap in pixels between connectable line segments
+
+    # 0. Get dimensions for our image
+    imshape = image.shape
+    imx = imshape[1]
+    imy = imshape[0]
+
+    # 1. Grayscale the image
+    gray = ut.grayscale(image)
+        
+    # 2. Apply Gaussian smoothing
+    blur_gray = ut.gaussian_blur(gray, kernel_size)
+
+    # 3. Apply Canny edge detection
+    edges = ut.canny(blur_gray, low_threshold, high_threshold)
+
+    # 4. Apply region of interest mask
+    vertices = ut.getVertices(image,vertextype)    # get the vertices for this vertextype
+    masked_edges, mask = ut.region_of_interest(edges,vertices)
+    
+    # 5. Apply Hough Transform
+    lines = ut.hough_lines(masked_edges, rho, theta, threshold, min_line_length, max_line_gap)
+
+    # 6. Use slope and position to separate left and right lines
+    leftlines,rightlines = ut.separateLeftRightLines(lines,imx,imy)
+
+    # 7. Average the left and right lines, and annotate the image
+    final_image = np.copy(image)
+    if(len(leftlines)):
+        leftx = np.array([0,imx/2-50],dtype=np.int)
+        lefty = ut.averageLines(leftlines,leftx).astype(np.int)
+        cv2.line(final_image, (leftx[0],lefty[0]), (leftx[1], lefty[1]), [255, 0, 0], 6)
+    if(len(rightlines)):
+        rightx = np.array([imx/2+50,imx],dtype=np.int)
+        righty = ut.averageLines(rightlines,rightx).astype(np.int)
+        cv2.line(final_image, (rightx[0],righty[0]), (rightx[1], righty[1]), [0, 255, 0], 6)
+            
+    # We are DONE!  The rest is just plotting of the steps in the pipeline.
+
+    plot_pipeline = False
+
+    if plot_pipeline:
+        print "Plotting pipeline ..."
+
+        # Plot the original image
+        plt.figure(1)
+        plt.imshow(image)
+        plt.title("Original Image")
+        plt.savefig("pipeline/fig0_original.png")
+
+        # Plot the grayscale image
+        plt.figure(2)
+        plt.imshow(gray,cmap='gray')
+        plt.title("Grayscale Image")
+        plt.savefig("pipeline/fig1_grayscale.png")
+
+        # Plot the blurred image
+        plt.figure(3)
+        plt.imshow(blur_gray,cmap='gray')
+        plt.title("Blurred Image")
+        plt.savefig('pipeline/fig2_blurred.jpg')
+
+        # Plot the canny edges
+        plt.figure(4)
+        plt.imshow(edges,cmap='gray')
+        plt.title("Canny Edges Image")
+        plt.savefig('pipeline/fig3_canny_edges.jpg')
+
+        # Plot the region mask
+        region = np.zeros_like(image)
+        pts = vertices.reshape((-1,1,2))
+        cv2.fillConvexPoly(region,pts,color=(255, 0, 0))
+        color_mask = cv2.addWeighted(image,1.0,region,0.3,0)
+        plt.figure(5)
+        plt.imshow(color_mask)
+        plt.title("Region Mask")
+        plt.savefig('pipeline/fig4_region_mask.jpg')
+
+        # Plot the masked canny edges
+        plt.figure(6)
+        plt.imshow(masked_edges,cmap='gray')
+        plt.title("Masked Canny Edges Image")
+        plt.savefig('pipeline/fig5_masked_edges.jpg')
+
+        # Plot the Hough lines 
+        plt.figure(7)
+        line_image = np.zeros_like(image)
+        ut.draw_lines(line_image, lines)
+        plt.imshow(line_image)
+        plt.title("Hough Lines")
+        plt.savefig('pipeline/fig6_hough_lines.jpg')
+
+        # Plot the hough lines after filtering for left and right lines
+        plt.figure(8)
+        lrline_image = np.zeros_like(image)
+        ut.draw_lines(lrline_image, leftlines, color=[255,0,0])
+        ut.draw_lines(lrline_image, rightlines, color=[0,255,0])
+        plt.imshow(lrline_image)
+        plt.title("Left-Right Hough Lines")
+        plt.savefig('pipeline/fig7_leftright_lines.jpg')
+
+        # Plot the averaged lines as the final image
+        plt.figure(9)
+        plt.imshow(final_image)
+        plt.title("Lane Lines")
+        plt.savefig('pipeline/fig8_lane_lines.jpg')
+
+    return final_image
+
+##########################################################################################
+
+if __name__ == '__main__':
+
+    # Do we want to process the test images, the test videos, or both
+    processimages = True
+    processvideos = True
+
+    if(processimages):
+        print('Processing images ...')
+        imagenames = os.listdir("test_images/")
+        vertextype = 'standard'
+        for imagename in imagenames[3:4]:    # process each image in the directory
+            fullname = os.path.join('test_images', imagename)
+            image = mpimg.imread(fullname)
+            procimg = process_image(image,vertextype)
+            
+            # Save the output figure
+            savename = os.path.join('test_images_output', imagename)
+            plt.figure()
+            plt.imshow(procimg)
+            plt.savefig(savename)
+            
+    if(processvideos):
+        print('Processing videos ...')
+        videos = os.listdir("test_videos/")
+        for video in videos:            # process each video in the directory
+            vertextype = 'standard'
+            if video=='challenge.mp4':      # the challenge video requires a different mask region
+                vertextype = 'challenge'
+            processed_video = os.path.join('test_videos_output', video)
+            videoclip = os.path.join('test_videos', video)
+            clip1 = VideoFileClip(videoclip)
+            processed_clip = clip1.fl_image(lambda image: process_image(image,vertextype)) # run the lane lines processor
+            get_ipython().run_line_magic('time', 'processed_clip.write_videofile(processed_video, audio=False)')    # save the output
+
+    print("DONE!!!")
+
+
+
+
---
+++ utilities.py
@@ -0,0 +1,107 @@
+# coding: utf-8
+#####################################################################
+# Imports
+#####################################################################
+import cv2
+from sklearn import linear_model        # for ransac linear regression
+import numpy as np
+
+######################################################################
+# Generic helper functions
+######################################################################
+
+def grayscale(img):
+    return cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    
+def canny(img, low_threshold, high_threshold):
+    return cv2.Canny(img, low_threshold, high_threshold)
+
+def gaussian_blur(img, kernel_size):
+    return cv2.GaussianBlur(img, (kernel_size, kernel_size), 0)
+
+def region_of_interest(img, vertices):
+    mask = np.zeros_like(img)   
+    ignore_mask_color = 255  
+    cv2.fillPoly(mask, vertices, ignore_mask_color)
+    masked_image = cv2.bitwise_and(img, mask)
+    return masked_image, mask
+
+def draw_lines(img, lines, color=[255, 0, 0], thickness=2):
+    for line in lines:
+        x1,y1,x2,y2 = line[0]
+        cv2.line(img, (x1, y1), (x2, y2), color, thickness)
+
+def hough_lines(img, rho, theta, threshold, min_line_len, max_line_gap):
+    lines = cv2.HoughLinesP(img, rho, theta, threshold, np.array([]), minLineLength=min_line_len, maxLineGap=max_line_gap)
+    return lines
+
+def weighted_img(img, initial_img, α=0.8, β=1., λ=0.):
+    return cv2.addWeighted(initial_img, α, img, β, λ)
+
+
+##############################################################################
+# Sub-Algorithms for detecting lane lines
+##############################################################################
+
+def separateLeftRightLines(lines,imx,imy):
+    # Given the image size imx,imy, sort lines into left and right using slope and location
+    left = []
+    right = []
+    da = 0.1
+    for line in lines:
+        x1,y1,x2,y2 = line[0]        
+        angle = np.arctan2(y2-y1,x2-x1)
+        if(angle<0):        #make all angles between 0 an pi.
+            angle += np.pi
+        if(  angle>(da*np.pi) and angle<(0.5-da)*np.pi and 0.5*(x1+x2)>imx/2+20):
+            right.append(line)
+        elif(angle>(0.5+da)*np.pi and angle<(1.0-da)*np.pi  and 0.5*(x1+x2)<imx/2-20):
+            left.append(line)
+    return left,right
+
+def sampleLine(x1,y1,x2,y2,ds): 
+    # Given a line defined by endpoints (x1,y1) ad (x2,y2), return points sampled along the line with spacing ds   
+    p1,p2 = np.array([x1,y1]),np.array([x2,y2])
+    length = np.linalg.norm(p2-p1);     # distance between endpoints
+    numpts = np.int(length/ds)+1
+    ptsx = np.linspace(x1,x2,numpts)    
+    ptsy = np.linspace(y1,y2,numpts)  
+    return np.vstack((ptsx,ptsy))
+
+def averageLines(lines,xvals):
+    # Given lines, compute a single representative line using ransac linear regression to reject outliers, and evaluate that line for
+    # the given xvals. 
+    # First, sample points from lines
+    allpts = np.empty((2,0), np.float)
+    for line in lines:
+        pts = sampleLine(*line[0],1.0)
+        allpts = np.append(allpts,pts,axis=1)
+    # Then perform the regression, evaluate for xvals, and return the corresponding yvals 
+    # lr = linear_model.LinearRegression()
+    lr = linear_model.RANSACRegressor()
+    lr.fit(allpts[0,:].reshape(-1, 1), allpts[1,:].reshape(-1, 1))
+    return lr.predict(xvals.reshape(-1, 1))
+
+def getVertices(image,vertextype):
+    # The challenge.mp4 movie requires a different vertex set to define the mask region.
+    imshape = image.shape
+    imx = imshape[1]
+    imy = imshape[0]
+
+    if vertextype=='standard':
+        dx = 30
+        dy = 65
+        dxtop = 60
+        return np.array([[(dx,imy),(imx/2-dxtop, imy/2+dy), (imx/2+dxtop, imy/2+dy), (imx-dx,imy)]], dtype=np.int32)
+    elif vertextype=='challenge':
+        dx = 180
+        dy = 100
+        dxtop = 90
+        dybot = 60
+        return np.array([[(dx,imy-dybot),(imx/2-dxtop, imy/2+dy), (imx/2+dxtop, imy/2+dy), (imx-dx,imy-dybot)]], dtype=np.int32)
+    else:
+        print('ERROR: unknown vertex type {} in getVertices()'.format(vertextype))
+        assert(False)
+
+
+
